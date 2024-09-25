9a669636c5c14cd7a4754c095ea3af41b1a68208
zackosowski <zacharyo@mystma.org>
2024-09-25 09:30:33 -0500

    Animal Survey Turn in

---
+++ Unit 2 - Functions and Variables/practice_sept-24.py
@@ -0,0 +1,5 @@
+print("Welcome to WORD SMASH!")                 #Welcome message
+word_one = input("Enter the first word!\n>")    #Ask for first word
+word_two = input("Enter the second word!\n>")   #Ask for second word
+print(word_one + word_two)                      #Concatenate & print
+
