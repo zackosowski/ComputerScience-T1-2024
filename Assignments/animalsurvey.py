156fbf084eebf04c90044512f9f0bd6551fe833f
zackosowski <zacharyo@mystma.org>
2024-09-25 09:32:01 -0500

    animal with comments

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,8 +1,8 @@
-animal = input("What is your favorite animal?\n>")
-why = input("What is the " + animal + " your favorite?\n>")
-color = input("What color is the " + animal + "?\n>")
-location = input("Where does the " + animal + " live?\n>")
-diet = input("What does the " + animal + " eat?\n>")                 
+animal = input("What is your favorite animal?\n>") # Ask for animal
+why = input("What is the " + animal + " your favorite?\n>") #Ask why
+color = input("What color is the " + animal + "?\n>")   #Ask about color
+location = input("Where does the " + animal + " live?\n>")  #Ask about location
+diet = input("What does the " + animal + " eat?\n>")    #Ask about what it eats
 
-
+#Print the summary
 print("Your favorite animal is the " + animal + " because " + why + ". It is " + color + ", eats " + diet + ", and lives in " + location + "!")
