a04dd0a92eadf88189e058ab5cb00b00ba76c760
zackosowski <zacharyo@mystma.org>
2024-09-26 09:34:24 -0500

    simple mad lib

---
+++ Assignments/simplemadlib.py
@@ -0,0 +1,25 @@
+# There once was a guy names [name]
+# and he went to [city] to
+# buy a [animal].
+# The [animal] was viscous, so he 
+# brought it to the [place].
+# [exclamation]
+
+
+#Ask for a guy's name
+name = input("Enter a guy's name\n>")
+
+#Ask for a city
+city = input("Enter the name of a city\n>")
+
+#Ask for an animal
+animal = input("Enter an animal\n>")
+
+#Ask for a place
+place = input("Enter a place\n>")
+
+#Ask for an exclamation
+exclamation = input("Enter something you would shout when something crazy happens\n>")
+
+#Print the completed mad lib
+print("There once was a guy named " + name + " and he went to " + city + " to buy a " + animal + ". The " + animal + " was vicious, so he took it to the " + place + ". " + exclamation + "!")
